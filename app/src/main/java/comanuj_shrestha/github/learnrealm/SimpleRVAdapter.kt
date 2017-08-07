f4a0784aab15d7fb362e1ee872295b25abd0c5f3
Anuj-Shrestha <anujshrestha@lftechnology.com>
2017-08-07 17:21:29 +0545

    LR-2 Add more data" ;

--- app/src/main/java/comanuj_shrestha/github/learnrealm/SimpleRVAdapter.kt
+++ app/src/main/java/comanuj_shrestha/github/learnrealm/SimpleRVAdapter.kt
@@ -12,7 +12,7 @@ import android.view.LayoutInflater
  * Created by anuj on 8/7/17.
  */
 class SimpleRVAdapter: RecyclerView.Adapter<SimpleViewHolder>() {
-    private val movies = arrayOf("1", "2", "3")
+    private val movies = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpleViewHolder {
         var view = LayoutInflater.from(parent.context).inflate(R.layout.view_recyclerview_row, parent, false)
